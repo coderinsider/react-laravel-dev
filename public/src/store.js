03792dfd601583292b81c907e77f93b26eda72e1
Myat Ko Ko <coderinsider1@gmail.com>
2022-04-02 17:28:08 +0630

    Adding redux and react-redux

---
+++ public/src/store.js
@@ -0,0 +1,6 @@
+import { createStore, combineReducers } from 'redux';
+
+const reducers = {};
+const rootReducers = combineReducers(reducers);
+
+export const configureStore = () => createStore(rootReducers);
