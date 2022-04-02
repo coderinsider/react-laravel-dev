09294efadc0436fe5f271ae509f281f876ccbe57
Myat Ko Ko <coderinsider1@gmail.com>
2022-04-02 17:06:33 +0630

    Creating product get api

---
+++ server/app/Http/Controllers/ProductListsController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Products;
+class ProductListsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct(Products $products) {
+        $this->products = $products;
+        $this->totalReocrd = 6;
+    }
+    public function index()
+    {
+        $findRecords = $this->products->paginate($this->totalReocrd);
+
+        $dataArray = [];
+        foreach($findRecords as $each => $value) {
+            $dataArray[$each] = [
+                "display_order" => $value->id,
+                "pack_id" => $value->pack_id,
+                "pack_name" => $value->pack_name,
+                "pack_description" => $value->pack_description,
+                "pack_type" => $value->pack_type,
+                "total_credit" => $value->total_credit,
+                "tag_name" => $value->tag_name,
+                "validity_month" => $value->validity_month,
+                "pack_price" => $value->pack_price,
+                "newbie_first_attend" => $value->newbie_first_attend,
+                "newbie_addition_credit" => $value->newbie_addition_credit,
+                "newbie_note" => $value->newbie_note,
+                "pack_alias" => $value->pack_alias,
+                "estimate_price" => $value->estimate_price,
+            ];
+        }
+        return response([
+            'errorCode' => 0,
+            'message' => 'Route request success',
+            'status' => true, 
+            'data' => [
+                'total_item' => $this->totalReocrd,
+                'total_page' => $findRecords->lastPage(),
+                'mem_tier' => 'newbie',
+                'total_expired_class' => 0,
+                'current_page' => $findRecords->currentPage(),
+                'pack_list'=> $dataArray
+            ]
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\ProductListsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,5 +21,5 @@ use App\Http\Controllers\API\RegisterController;
 Route::post('register', [RegisterController::class, 'register']);
 Route::post('login', [RegisterController::class, 'login']);
 Route::prefix('codigo')->middleware('auth:api')->group( function () {
-    Route::get('products', [RegisterController::class, 'products']);
+    Route::get('products/records', [ProductListsController::class, 'index']);
 });
